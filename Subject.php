b330743aaab3cc4d8bb6e925ca1473af01964637
GacitaStephanie <stephaniegacita18@gmail.com>
2024-07-01 19:55:45 +0800

    Subject.php

--- Subject.php
+++ Subject.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_id', 'subject_code', 'name', 'description', 'instructor', 'schedule',
+        'prelims', 'midterms', 'pre_finals', 'finals', 'date_taken', 'average_grade', 'remarks',
+    ];
+
+    protected $casts = [
+        'grades' => 'array', // Cast grades as array
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
