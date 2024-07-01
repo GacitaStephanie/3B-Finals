0d5b7f567332bc9fb73e504ec4f60f3450a7b248
GacitaStephanie <stephaniegacita18@gmail.com>
2024-07-01 19:55:23 +0800

    Student.php

--- Student.php
+++ Student.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'firstname', 'lastname', 'birthdate', 'sex', 'address', 'year', 'course', 'section',
+    ];
+    public function subjects()
+    {
+        return $this->hasMany(Subject::class);
+    }
 }
